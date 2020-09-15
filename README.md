3643181e7b4903b60c35a07fbe1d5c562b97ebf2
Raman Aryana <14raryana@gmail.com>
2020-09-15 14:50:24 -0400

    Fully functional password Generator

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # passwordGenerator
 Homework 3
+
+## What is passwordGenerator
+The purpose of this program is to take user input and based on that randomly generate a password that the user can then use for whatever they intended to use it for.  Since the passwordGenerator is random the strength of the passwords that it creates are much higher than that of normal passwords that people put together (such as a birthday).
+
+## Run through of program
+
+1. ### Password criteria
+The user is asked to input the number of characters they wish their password to be as long as that number is between 8 and 128 inclusive.  If the user skips this portion and tries to generate a password an alert will pop up informing the user that a number of characters is required.  Under the length number input box the user also has 4 options of password criteria to choose from.  They can pick from any combination of these criteria as long as at least one is selected.  If they try to continue without picking one an alert will appear telling them that they must pick at least one before they continue.
+
+2. ### Generate Password
+Once all the criteria sections have been filled out properly the user can then click on the big red `Generate Password` button.  When this button is pressed several things happen in the background (explained later) however 3 new things appear on the screen.  First and foremost is the randomly generated password with at least 1 character of each criteria that has been picked.  To ensure that things would not be missing from the generated password just by chance, I run through the password array.  If anything from the selected criteria is missing the computer will pick a random index within the password array and add a random character of the missing criteria into the password.  It does this for each criteria that happens to be missing and makes sure that important characters do not get overwritten.  All this happens in a function called `passwordValidationFunction()` and will continue until at least 1 character of each required criteria is added to the password.  However, the function will *only* run if something happens to be missing. 
+
+### Extra buttons
+The other 2 things that appear once the Generate Password button has been pressed are a left arrow button and a copy to clipboard button.  All extra buttons that appear have tooltips within their code, so if you hover your mouse over them a tiny message will appear next to your cursor telling you what it does.
+
+### Previous Button (Left arrow button)
+Every password that is generated is added to an array that empties everytime the page is refreshed.  There is no limit to the number of passwords that are added to the array so a user can create multiple passwords and click the previous password button to go back and view all the passwords that they have made.  When the previous button is pressed another button appears to the right which is called the next button.  This button works similarly to the previous button but instead allows you to go forward within the list of saved passwords (does not creat new password).  Once the user has reached the end of the list of stored passwords an alert informs them that they have viewed all created passwords and brings them back to either the most current password in the list or oldest password in the list depending on if they are pressing the previous button or next button respectively. 
+
+### Copy to Clipboard Button
+This button is pretty self explanatory.  Upon clicking this button the displayed password is automatically copied to the users clipboard so they are able to take it somewhere else and paste it.
+
+## Link to GitHub Pages for this site:
--- passwordGenFunctions.js
+++ passwordGenFunctions.js
@@ -2,286 +2,267 @@ var lowercaseAlphabetArray = "abcdefghijklmnopqrstuvwxyz".split("");
 var uppercaseAlphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numberArray = "0123456789".split("");
 var specialCharacterArray = "~!@#$%^&*()-_=+[{]}:;|".split("");
+var arrayOfArrays = [uppercaseAlphabetArray, lowercaseAlphabetArray, numberArray, specialCharacterArray];
+var numberOfCharacters;
 var finalArray;
 var password;    
-var numberOfCharacters;
-var joinedPassword;
+var uppercase;
+var lowercase;
+var numbers;
+var specialCharacters;
 const MIN_NUMBER_OF_CHARACTERS = 8;
 const MAX_NUMBER_OF_CHARACTERS = 128;
+var storedPassArray = new Array();
 
 
+function numberOfCharacterCheck() {
+    var characters = document.getElementById("numberOfCharacters").value;
+    if(characters == "") {
+        alert("Must choose the number of characters you wish your password to be");
+    }
+    else if (characters < MIN_NUMBER_OF_CHARACTERS || characters > MAX_NUMBER_OF_CHARACTERS) {
+        alert("Must choose a number between 8 and 128 inclusive");
+    }
+    else {
+        numberOfCharacters = parseInt(characters);
+        criteriaCheck();
+    }
+}
 
+function criteriaCheck() {
+    uppercase = document.getElementById("uppercase").checked;
+    lowercase = document.getElementById("lowercase").checked;
+    numbers = document.getElementById("number").checked;
+    specialCharacters = document.getElementById("specialCharacter").checked;
 
-// var characters = document.getElementById("numberOfCharacters").value;
+    if(!uppercase && !lowercase && !numbers && !specialCharacters) {
+        alert("Must pick at least one of the checkboxes to generate your password");
+    }
+    else {
+        finalArray = new Array();
 
-//  var num = Math.floor(Math.random()*finalArray.length);
-
- function characterFunction() {
-  //  console.log("password character function");
-
-  var characters = document.getElementById("numberOfCharacters").value;
-
-  if (characters == "") {
-    alert("You must choose the number of characters for your password");
-  }
-  else if (characters < MIN_NUMBER_OF_CHARACTERS || characters > MAX_NUMBER_OF_CHARACTERS) {
-    alert("You must choose a number between 8 and 128 inclusive");
-  }
-  else if (characters >= MIN_NUMBER_OF_CHARACTERS && characters <= MAX_NUMBER_OF_CHARACTERS) {
-    numberOfCharacters = parseInt(characters);
-    // var password = new Array(numberOfCharacters);
-    // console.log("it worked");
-    password = new Array();
-    finalArray = new Array();
-    passwordCriteriaFunction();
-  }
-  // return numberOfCharacters;
- }
+        passwordCriteriaFunction();
+    }
+}
 
 function passwordCriteriaFunction() {
-
-var uppercase = document.getElementById("uppercase").checked;
-var lowercase = document.getElementById("lowercase").checked;
-var numbers = document.getElementById("number").checked;
-var specialCharacters = document.getElementById("specialCharacter").checked;
-// finalArray.length = 0;
-
-  // console.log("criteria function");
-
-  //1 -----
-  if (!uppercase && !lowercase && !numbers && !specialCharacters) {
-    alert("must choose at least one criteria using the checkboxes")
-  }
-  //2 u
-  else if (uppercase && !lowercase && !numbers && !specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
+    if(uppercase) {
+        finalArrayFunction(uppercaseAlphabetArray);
     }
-    console.log("final array length " + finalArray.length);
-    console.log("uppercase");
-
-}
-  //3 l
-  else if (!uppercase && lowercase && !numbers && !specialCharacters) {
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
+    if(lowercase) {
+        finalArrayFunction(lowercaseAlphabetArray);
     }
-    console.log("final array length " + finalArray.length);
-
-    console.log("lowercase");
-
-  }
-  //4 n
-  else if (!uppercase && !lowercase && numbers && !specialCharacters) {
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
+    if(numbers) {
+        finalArrayFunction(numberArray);
     }
-    console.log("final array length " + finalArray.length);
-
-    console.log("number");
-
-  }
-  //5 s
-  else if (!uppercase && !lowercase && !numbers && specialCharacters) {
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
+    if(specialCharacters) {
+        finalArrayFunction(specialCharacterArray);
     }
-    console.log("final array length " + finalArray.length);
-
-    console.log("special");
-
-  }
-  //6 ul
-  else if (uppercase && lowercase && !numbers && !specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase lowercase");
-
-  }
-  //7 un
-  else if (uppercase && !lowercase && numbers && !specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase number");
-
-  }
-  //8 us
-  else if (uppercase && !lowercase && !numbers && specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase special");
-
-  }
-  //9 ln
-  else if (!uppercase && lowercase && numbers && !specialCharacters) {
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("lowercase number");
-
-  }
-  //10 ls
-  else if (!uppercase && lowercase && !numbers && specialCharacters) {
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("lowercase special");
-
-  }
-  //11 ns
-  else if (!uppercase && !lowercase && numbers && specialCharacters) {
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("number special");
-
-  }
-  //12 uln
-  else if (uppercase && lowercase && numbers && !specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase lowercase number");
-
-  }
-  //13 lns
-  else if (!uppercase && lowercase && numbers && specialCharacters) {
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("lowercase number special");
-
-  }
-  //14 uns
-  else if (uppercase && !lowercase && numbers && specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase number special");
-
-  }
-  //15 uls
-  else if (uppercase && lowercase && !numbers && specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase lowercase special");
-
-  }
-  //16 ulns
-  else if (uppercase && lowercase && numbers && specialCharacters) {
-    for (i = 0; i < uppercaseAlphabetArray.length; i++) {
-      finalArray.push(uppercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < lowercaseAlphabetArray.length; i++) {
-      finalArray.push(lowercaseAlphabetArray[i]);
-    }
-    for (i = 0; i < numberArray.length; i++) {
-      finalArray.push(numberArray[i]);
-    }
-    for (i = 0; i < specialCharacterArray.length; i++) {
-      finalArray.push(specialCharacterArray[i]);
-    }
-    console.log("final array length " + finalArray.length);
-
-    console.log("uppercase lowercase number special");
-
-  }
-  console.log(finalArray);
-  arrayFunction(finalArray);
+    // console.log(finalArray);
+    // console.log(numberOfCharacters);
+    passwordGenerationFunction();
 }
 
-function arrayFunction(finalArray) {
-  console.log(finalArray);
-  passwordFunction(finalArray);
+function finalArrayFunction(array) {
+    // for (i = 0; i < array.length; i++) {
+        finalArray = finalArray.concat(array)
+    // }
+    // console.log(finalArray);
+    // return finalArray;
 }
 
-function passwordFunction(finalArray) {
-  for (i = 0; i < numberOfCharacters; i++) {
-    var randomIndex = Math.floor(Math.random()*finalArray.length);
-    password.push(finalArray[randomIndex]);
-    console.log(i);
-  }
-  console.log(password);
-  
-  joinedPassword = password.join("");
-  console.log(joinedPassword);
-  generatePasswordForUser(joinedPassword);
-  // console.log(password);
-  // console.log(numberOfCharacters);
+function passwordGenerationFunction() {
+    // console.log(finalArray);
+    password = new Array();
+    for (i=0; i<numberOfCharacters; i++) {
+        password.push(finalArray[Math.floor(Math.random() * finalArray.length)]);
+    }
+    // console.log("first password array " + password);
+    passwordValidationFunction();
+    // console.log("second password array " + password);
+    generatePasswordForUser();
 }
 
-function generatePasswordForUser(joinedPassword) {
-  document.getElementById("password").innerHTML = joinedPassword;
+function passwordValidationFunction() {
+    var uppercaseIncluded;
+    var lowercaseIncluded;
+    var numbersIncluded;
+    var specialCharactersIncluded;
+    if(uppercase) {
+        // console.log("checking uppercase " + uppercaseIncluded);
+        for (i=0; i<password.length && !uppercaseIncluded; i++) {
+            uppercaseIncluded = uppercaseAlphabetArray.includes(password[i]);
+            if(uppercaseIncluded) {
+                var upperRandomIndex = i;
+                // console.log("upperRandomIndex " + upperRandomIndex);
+            }
+        }
+        if(!uppercaseIncluded) {
+            // console.log("still no uppercase " + uppercaseIncluded);
+            var upperRandomIndex = Math.floor(Math.random() * password.length);
+            var randomUppercaseLetter = uppercaseAlphabetArray[Math.floor(Math.random() * uppercaseAlphabetArray.length)];
+            password.splice(upperRandomIndex,1,randomUppercaseLetter);
+            // console.log("upperRandomIndex " + upperRandomIndex);
+        }
+    }
+    if(lowercase) {
+        // console.log("checking lowercase " + lowercaseIncluded);
+        for (i=0; i<password.length && !lowercaseIncluded; i++) {
+            lowercaseIncluded = lowercaseAlphabetArray.includes(password[i]);
+            if(lowercaseIncluded) {
+                var lowerRandomIndex = i;
+                // console.log("lowerRandomIndex " + lowerRandomIndex);
+            }
+        }
+        if(!lowercaseIncluded) {
+            // console.log("still no lowercase " + lowercaseIncluded);
+            var lowerRandomIndex = Math.floor(Math.random() * password.length);
+            while(lowerRandomIndex == upperRandomIndex) {
+                lowerRandomIndex = Math.floor(Math.random() * password.length);
+            }
+            var randomLowercaseLetter = lowercaseAlphabetArray[Math.floor(Math.random() * lowercaseAlphabetArray.length)];
+            password.splice(lowerRandomIndex,1,randomLowercaseLetter);
+            // console.log("lowerRandomIndex " + lowerRandomIndex);
+        }
+    }
+    if(numbers) {
+        // console.log("checking numbers " + numbersIncluded);
+        for (i=0; i<password.length && !numbersIncluded; i++) {
+            numbersIncluded = numberArray.includes(password[i]);
+            if(numbersIncluded) {
+                var numberRandomIndex = i;
+                // console.log("numberRandomIndex " + numberRandomIndex);
+            }
+        }
+        if(!numbersIncluded) {
+            // console.log("still no numbers " + numbersIncluded);
+            var numberRandomIndex = Math.floor(Math.random() * password.length);
+            while(numberRandomIndex == upperRandomIndex || numberRandomIndex == lowerRandomIndex) {
+                numberRandomIndex = Math.floor(Math.random() * password.length);
+            }
+            var randomNumber = numberArray[Math.floor(Math.random() * numberArray.length)];
+            password.splice(numberRandomIndex,1,randomNumber);
+            // console.log("numberRandomIndex " + numberRandomIndex);
+        }
+    }
+    if(specialCharacters) {
+        // console.log("checking special characters " + specialCharactersIncluded);
+        for (i=0; i<password.length && !specialCharactersIncluded; i++) {
+            specialCharactersIncluded = specialCharacterArray.includes(password[i]);
+            if(specialCharactersIncluded) {
+                var specialCharacterRandomIndex = i;
+                // console.log("specialCharacterRandomIndex " + specialCharacterRandomIndex);
+            }
+        }
+        if(!specialCharactersIncluded) {
+            // console.log("still no special characters " + specialCharactersIncluded);
+            var specialCharacterRandomIndex = Math.floor(Math.random() * password.length);
+            while(specialCharacterRandomIndex == upperRandomIndex || specialCharacterRandomIndex == lowerRandomIndex || specialCharacterRandomIndex == numberRandomIndex) {
+                specialCharacterRandomIndex = Math.floor(Math.random() * password.length);
+            }
+            var randomSpecialCharacter = specialCharacterArray[Math.floor(Math.random() * specialCharacterArray.length)];
+            password.splice(specialCharacterRandomIndex,1,randomSpecialCharacter);
+            // console.log("specialCharacterRandomIndex " + specialCharacterRandomIndex);
+        }
+    }
+}
+
+function generatePasswordForUser() {
+    document.getElementById("password").innerHTML = password.join("");
+    // var lastPass = password;
+    // previousPasswordFunction();
 }
 
 
+// previous password function variables
+var generate = document.getElementById("generate");
+var clickcount = 0;
+var prevPass = document.getElementById("prevPass");
+var nextPass = document.getElementById("nextPass");
+var totalPasswordArray = new Array();
+// var totalPasswordArrayReverse = totalPasswordArray.splice().reverse();
+// totalPasswordArray.reverse();
+// var totalNumberOfPasswords = 0;
+var totalPasswordArrayIndex;
+// localStorage.setItem("storedPasswords", totalPasswordArray);
 
-//  var characters = getElementById("numberOfCharacters").value;
-// var numberOfCharacters = parseInt(characters);
-// var password = new Array(numberOfCharacters);
+generate.addEventListener("click",function() {
+    if(password) {
+    totalPasswordArray.push(password);
+    totalPasswordArrayIndex = totalPasswordArray.length - 1;
+    console.log(totalPasswordArrayIndex);
+    prevPass.style.visibility = "visible";
+    nextPass.disabled = true;
+    document.getElementById("copyPassword").style.visibility = "visible";
+    }
+});
+
+// previous password function 
+prevPass.addEventListener("click", function() {
+    nextPass.disabled = false;
+    totalPasswordArrayIndex--;
+
+    if(totalPasswordArrayIndex < 0) {
+        alert("went through all passwords");
+        totalPasswordArrayIndex = totalPasswordArray.length - 1;
+    }
+    else {document.getElementById("nextPass").style.visibility = "visible";}
+
+    document.getElementById("password").innerHTML = totalPasswordArray[totalPasswordArrayIndex].join("");
+    console.log(totalPasswordArrayIndex);
+    console.log(totalPasswordArray);
+});
+
+// next password function
+nextPass.addEventListener("click", function() {
+
+    totalPasswordArrayIndex++;
+
+    if(totalPasswordArrayIndex == totalPasswordArray.length) {
+        alert("went through all passwords");
+        totalPasswordArrayIndex = 0;
+        // document.getElementById("password").innerHTML = totalPasswordArray[totalPasswordArrayIndex];
+    }
+
+    document.getElementById("password").innerHTML = totalPasswordArray[totalPasswordArrayIndex].join("");
+    console.log(totalPasswordArrayIndex);
+    console.log(totalPasswordArray);
+
+});
+
+
+// // storing password function
+// var storePassBtn = document.getElementById("storePasswordBtn");
+// var printStoredPasswords = document.getElementById("printPass");
+// var storedPassArrayIndex = 0;
+// // console.log(storedPassArrayIndex);
+// var numberOfStoredPasswords = 0;
+
+// function storePasswordFunction() {
+//     numberOfStoredPasswords++;
+//     storePassBtn.textContent = "You have stored " + numberOfStoredPasswords + " password(s)";
+//     localStorage.setItem("password", password.join(""));
+//     storedPassArray.push(localStorage.getItem("password"));
+//     localStorage.setItem("storedPasswords", storedPassArray);
+// }
+
+// // print stored passwords
+// printStoredPasswords.addEventListener("click", function() {
+//     if (storedPassArrayIndex > storedPassArray.length - 1) {
+//         storedPassArrayIndex = 0;
+//         alert("You have gone through all of your stored passwords");
+//         document.getElementById("password").innerHTML = storedPassArrayIndex + 1 + ") " + storedPassArray[storedPassArrayIndex];
+//         storedPassArrayIndex++;
+//     }
+//     else {
+//         document.getElementById("password").innerHTML = storedPassArrayIndex + 1 + ") " + storedPassArray[storedPassArrayIndex];
+//         storedPassArrayIndex++;
+//     }
+// });
+
+// copy password to clipboard
+function copyPasswordFunction() {
+    var copyPassword = document.getElementById("password");
+    copyPassword.select();
+    document.execCommand("copy");
+}
